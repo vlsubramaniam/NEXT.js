5eb3c73bfd0fb5a4890419faf85c70db4058cdcf
Subramaniam <subramaniam.vls@gmail.com>
2021-09-07 17:09:23 +0530

    07-SEP-2021

---
+++ 03-pre-render/pages/[pid].js
@@ -0,0 +1,60 @@
+import path from 'path';
+import fs from 'fs/promises';
+
+const ProductDetailPage = ({ product }) => {
+  // if (!product) {
+  //   return <p>Loading...</p>;
+  // }
+  return (
+    <>
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+    </>
+  );
+};
+
+/************ NOT WORKING ==> Error - Unable to resolve module fs/promises ***************/
+// export async function getData() {
+//   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+//   const JSONData = await fs.readFile(filePath);
+//   const data = JSON.parse(JSONData);
+//   return data;
+// }
+
+export async function getStaticProps(context) {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const JSONData = await fs.readFile(filePath);
+  const data = JSON.parse(JSONData);
+  const { params } = context;
+  const productId = params.pid;
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const JSONData = await fs.readFile(filePath);
+  const data = JSON.parse(JSONData);
+  const productIds = data.products.map((product) => ({
+    params: {
+      pid: product.id,
+    },
+  }));
+
+  return {
+    paths: productIds,
+    /* fallback: true /* Tells Next.js the paths that are not availble are valid and should be 
+    generated JIT & not pregenerated. Example there may be instances where some of the pages are not visited often
+    and it is a waste of time to prerender all the pages
+    */
+    // fallback: false,
+    fallback: 'blocking',
+  };
+}
+
+export default ProductDetailPage;
